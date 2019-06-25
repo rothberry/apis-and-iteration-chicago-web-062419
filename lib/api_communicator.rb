7a924d228069d24b8a02ecd7b09209ceaa00af8d
Philip Roth <50246817+rothberry@users.noreply.github.com>
2019-06-25 17:22:00 -0500

    solved star wars

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,24 +2,38 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character_name)
-  #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
+def get_api_hash(string=nil)
+  if string == nil
+    response_string = RestClient.get('http://www.swapi.co/api/people/')
+    response_hash = JSON.parse(response_string)
+    response_hash
+  else
+    response_string = RestClient.get(string)
+    response_hash = JSON.parse(response_string)
+    response_hash
+  end
+end
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+def get_character_movies_from_api(character_name)
+    
+  film_array = []
+  get_api_hash["results"].each do |x|
+    if x["name"].downcase == character_name.downcase
+      x["films"].collect do |film|
+        film_response_hash = get_api_hash(film)
+        film_array << film_response_hash["title"]
+      end
+    end
+  end
+  return film_array
 end
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+  counter = 1
+  films.each do |film|
+    puts "#{counter}. #{film}"
+    counter += 1
+  end
 end
 
 def show_character_movies(character)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,21 @@
 def welcome
   # puts out a welcome message here!
+  puts "May the force be wih you..."
 end
 
 def get_character_from_user
   puts "please enter a character name"
   # use gets to capture the user's input. This method should return that input, downcased.
+  character_input = gets.chomp
 end
+
+# get_character_from_api
+  # iterate over the response hash to find the collection of `films` for the given
+  #   `character`
+  # collect those film API urls, make a web request to each URL to get the info
+  #  for that film
+  # return value of this method should be collection of info about each film.
+  #  i.e. an array of hashes in which each hash reps a given film
+  # this collection will be the argument given to `print_movies`
+  #  and that method will do some nice presentation stuff like puts out a list
+  #  of movies by title. Have a play around with the puts with other info about a given film.
